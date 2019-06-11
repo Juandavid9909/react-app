a49c6fdaef8f4c900862782cceec0e84ccd21fc3
Juan David <juandavid918@gmail.com>
2019-06-11 11:46:06 -0500

    Finalizado login y TodoList

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import fire from './config/Fire';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Login from './components/Login';
+import Home from './components/Home';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: {}
+    }
+  }
+
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener() {
+    fire.auth().onAuthStateChanged((user) => {
+      console.log(user);
+
+      if(user) {
+        this.setState({ user });
+        localStorage.setItem('user', user.uid);
+      }
+      else {
+        this.setState({ user:null });
+        localStorage.removeItem('user');
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        { this.state.user ? (<Home />) : (<Login />) }
+      </div>
+    );
+  }
 }
 
 export default App;
